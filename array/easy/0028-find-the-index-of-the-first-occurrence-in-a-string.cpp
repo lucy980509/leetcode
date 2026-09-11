a050b67912dbe50aed53af2f465f0e2f212a52e5
Sumin Sim <lucy980509@gmail.com>
2026-09-11 16:01:16 -0400

    Add strStr function for substring search
    Implement strStr function to find the index of the first occurrence of a substring in a string.

---
+++ array/easy/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        if (needle.size() > haystack.size()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.size() - needle.size(); i++) {
+
+            int matched = 0;
+
+            for (int j = 0; j < needle.size(); j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+                matched++;
+            }
+
+            if (matched == needle.size()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
