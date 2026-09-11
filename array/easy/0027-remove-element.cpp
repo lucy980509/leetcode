3da3e7252952bbfa9ff494a2d2b0c13ff987148d
Sumin Sim <lucy980509@gmail.com>
2026-09-11 15:43:33 -0400

    Create 0027-remove-element.cpp

---
+++ array/easy/0027-remove-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int write = 0;
+
+        for (int i = 0; i < nums.size(); i++){
+            if (val != nums[i]){
+                nums[write] = nums[i];
+                write ++;
+            }
+        }
+
+        return write;
+
+    }
+};
